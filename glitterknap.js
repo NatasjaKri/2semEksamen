9c053605d05e4c03c6ffb1cde87d2f38c4e4a578
ChubbiBunni <sara468178@gmail.com>
2025-06-01 16:33:52 +0200

    Sparkle time

--- glitterknap.js
+++ glitterknap.js
@@ -1,22 +1,25 @@
-const glitterButton = document.querySelector('.glitter-button');
+// JavaScript Document
+const glitterButton = document.querySelector('.glitter-button'); //finder den rigtige knap
 
 function createSparkleOnButton(button) {
   const sparkle = document.createElement('div');
-  sparkle.classList.add('sparkle');
+  sparkle.classList.add('sparkle'); //ny class, bruges i CSS
 
+	//finder den rigtige position og størrelse på knappen
   const rect = button.getBoundingClientRect();
   const offsetX = Math.random() * rect.width;
   const offsetY = Math.random() * rect.height;
 
-  sparkle.style.left = `${rect.left + offsetX}px`;
-  sparkle.style.top = `${rect.top + offsetY}px`;
+	//placere glitter randomly.
+  sparkle.style.left = ${rect.left + offsetX}px;
+  sparkle.style.top = ${rect.top + offsetY}px;
 
   document.body.appendChild(sparkle);
 
-  setTimeout(() => sparkle.remove(), 800);
+  setTimeout(() => sparkle.remove(), 800); //add'er timeout for animation for at undgå at have for mange elementer til at kører på samme tid. 
 }
 
-// Loop sparkles every 400ms
+// Loop glitter hver 400ms
 setInterval(() => {
   createSparkleOnButton(glitterButton);
-}, 400);// JavaScript Document
+}, 400);
