56a17b130d0ac58f6b0e80d712b2bbbb2d8f88a7
ChubbiBunni <sara468178@gmail.com>
2025-06-01 16:48:42 +0200

    glitter-knap færdig
    endelig version af glitterknap. Bruges permanent, med disney vibes. Bubble-vibes er også at finde, men greyed out

--- glitterknap.js
+++ glitterknap.js
@@ -1,25 +1,24 @@
-// JavaScript Document
 const glitterButton = document.querySelector('.glitter-button'); //finder den rigtige knap
 
 function createSparkleOnButton(button) {
   const sparkle = document.createElement('div');
   sparkle.classList.add('sparkle'); //ny class, bruges i CSS
 
-	//finder den rigtige position og størrelse på knappen
+  //finder den rigtige position og størrelse på knappen
   const rect = button.getBoundingClientRect();
   const offsetX = Math.random() * rect.width;
   const offsetY = Math.random() * rect.height;
 
-	//placere glitter randomly.
-  sparkle.style.left = ${rect.left + offsetX}px;
-  sparkle.style.top = ${rect.top + offsetY}px;
+  //placere glitter randomly + tilføjet scroll-behaviour hvis det skulle bruges senere. 
+  sparkle.style.left = `${rect.left + offsetX + window.scrollX}px`;
+  sparkle.style.top = `${rect.top + offsetY + window.scrollY}px`;
 
   document.body.appendChild(sparkle);
 
-  setTimeout(() => sparkle.remove(), 800); //add'er timeout for animation for at undgå at have for mange elementer til at kører på samme tid. 
+  setTimeout(() => sparkle.remove(), 800); //fjerner glitter efter animation
 }
 
 // Loop glitter hver 400ms
 setInterval(() => {
   createSparkleOnButton(glitterButton);
-}, 400);
+}, 700);
